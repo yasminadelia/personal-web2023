718455a5a4d8ef7140e1c36d0138698900877c90
yasminadeliaMIG <yasminadelia11@gmail.com>
2023-01-14 19:29:25 +0700

    style: config web theme in tailwind, remove unused css, add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      theme1: "#FFFCF3",
+      theme2: "#FFF7B4",
+      theme3: "#F6D79C",
+      theme4: "#FFCD6F",
+    },
+    fontFamily: {
+      old: ["Old Standard TT", "serif"],
+      sourcecode: ["Source Code Pro", "monospace"],
+      hind: ["Hind Guntur", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
