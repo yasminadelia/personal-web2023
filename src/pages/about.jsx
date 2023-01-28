50182704cf39b795c054e401f0cec9d8dfce613e
yasminadeliaMIG <yasminadelia11@gmail.com>
2023-01-28 23:51:44 +0700

    feat: add content in about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,29 +1,62 @@
 import Head from "next/head";
 import React from "react";
 import Layout from "../components/layout/Layout";
+import ProfileImage from "../assets/images/profile.jpg";
+import Image from "next/image";
 
 const About = () => {
   return (
     <Layout>
       <Head>
         <title>About me</title>
-        <meta name="description" content="Yasmin Adelia's personal website" />
+        <meta name="description" content="About Yasmin" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="main">
-        <h1 className="text-gray-100 text-[3rem] mb-6 leading-relaxed">
-          Hello, <br /> I&apos;m
-          <span className="p-2 m-2 bg-theme2 rounded-full border drop-shadow-lg">
-            Yasmin Adelia
-          </span>
-        </h1>
+      <main className="main ">
+        <div className="flex flex-row items-start">
+          <div className="w-2/3">
+            <h1 className="text-gray-100 text-[2rem] mb-6 leading-relaxed">
+              Hi there, you’ve visited About Me page.
+            </h1>
 
-        <p className="w-3/4">
-          I&apos;m a computer science fresh graduate, based in Jakarta,
-          Indonesia. Currently working as a Front-end Developer using NextJS and
-          TailwindCSS.
-        </p>
+            <p className="w-11/12">
+              So, let me introduce myself a little bit more. I&apos;m Yasmin,
+              others also called me as yas, min, asmin, yamin, mimin, minmin. I
+              graduated as a Bachelor of Computer Science in September 2022.
+              <br />
+              <br />
+              Currently, I really enjoy my life as a fully-remote front-end
+              developer. I&apos;ve known and used ReactJS since February 2021
+              during undergraduate course, internship, and current job, but
+              I&apos;m still learning until now through online courses,
+              projects, tweets, tiktok vids, or in other words{" "}
+              <strong>✨the internet✨</strong>. I&apos;m also planning to
+              expand my skill in back-end development, so wish me luck.
+              <br />
+              <br />
+              Aside from coding stuffs, I love photography, coffee, Taylor
+              Swift, K-Drama, romanticizing my quite-boring-life on social
+              media, and music! Checkout my spotify playlists{" "}
+              <a
+                href="https://open.spotify.com/user/yasminadelia?si=f6aafec41c384d3a"
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                here
+              </a>
+              .
+            </p>
+          </div>
+          <Image
+            src={ProfileImage}
+            alt="profile photo"
+            className="mt-4 w-1/3 object-contain border border-black rounded-xl drop-shadow-lg"
+          ></Image>
+        </div>
+
+        {/* TODO: add contact */}
+        {/* <div>You can contact me at</div> */}
       </main>
     </Layout>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import Experiences from "../components/home/Experiences";
-import ProjectSection from "../components/home/Projects";
-import Skills from "../components/home/Skills";
+import ProjectSection from "../components/home/ProjectSection";
 import Layout from "../components/layout/Layout";
+import Blobs1 from "../assets/images/blobs1.svg";
+import ExperienceSection from "../components/home/ExperienceSection";
+import SkillSection from "../components/home/SkillSection";
 
 export default function Home() {
   return (
@@ -16,21 +17,30 @@ export default function Home() {
       </Head>
 
       <main className="main">
-        <h1 className="text-gray-100 text-[3rem] mb-6 leading-relaxed">
+        <h1 className="text-gray-100 text-[3rem] mb-6 leading-relaxed z-10">
           Hello, <br /> I&apos;m
           <span className="p-2 m-2 bg-theme2 rounded-full border drop-shadow-lg">
             Yasmin Adelia
           </span>
         </h1>
 
-        <p className="w-3/4">
+        <p className="w-3/4 z-10">
           I&apos;m a computer science fresh graduate, based in Jakarta,
           Indonesia. Currently working as a Front-end Developer using NextJS and
           TailwindCSS.
         </p>
 
-        <Experiences />
-        <Skills />
+        {/* <img className={"absolute"} style={{ width: "20px" }} src={Blobs1} /> */}
+        {/* <Image
+          src={Blobs1}
+          alt="blobs svg"
+          width={700}
+          className="absolute top-20 -right-[15rem] z-0"
+          // style={{ float: "right" }}
+        ></Image> */}
+
+        <ExperienceSection />
+        <SkillSection />
         <ProjectSection />
       </main>
     </Layout>
