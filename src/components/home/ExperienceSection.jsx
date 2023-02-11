1061e1b00111934118f352de8ed898fa1ed8a7a0
yasminadeliaMIG <yasminadelia11@gmail.com>
2023-02-11 23:37:40 +0700

    chore: modify contents in experience and project section

--- src/components/home/ExperienceSection.jsx
+++ src/components/home/ExperienceSection.jsx
@@ -3,20 +3,25 @@ import React from "react";
 const ExperienceSection = () => {
   return (
     <section className="z-10 my-8">
-      <h2 className="my-4 rounded-full border bg-theme2 py-2 px-4 text-2xl drop-shadow-lg">
+      <h2 className="my-8 rounded-full border bg-theme2 py-2 px-4 text-2xl drop-shadow-lg">
         Work Experiences
       </h2>
       <ul className="space-y-4">
-        {experiences.map(({ role, company, period, description }) => (
+        {experiences.map(({ role, company, period, description, tools }) => (
           <li key={company} className="flex flex-row space-x-2">
             <p className="text-4xl">🌻</p>
             <div>
-              <h3 className="text-lg font-semibold">{role}</h3>
-              <p className="text-md">
+              <h3 className="mb-3 text-lg font-semibold">{role}</h3>
+              <p className="text-md mb-2">
                 <span className="mr-2 font-semibold">{company}</span> |{" "}
                 <span className="ml-2">{period}</span>
               </p>
-              <p className="text-sm">{description}</p>
+              <p className="mb-2 text-justify text-sm">{description}</p>
+              {tools && (
+                <p className="text-justify text-sm">
+                  <strong>Tools:</strong> {tools}
+                </p>
+              )}
             </div>
           </li>
         ))}
@@ -33,20 +38,22 @@ const experiences = [
     company: "Mitramas Infosys Global",
     period: "Sep 2022 - Now",
     description:
-      "Develop the company's website using Next.js, TailwindCSS, and Ant Design components.",
+      "Developed the front-end of a website that used for manage recruitment, create resume, and manage employee payslip.",
+    tools: "Next.js, TailwindCSS, Ant Design components, React PDF",
   },
   {
     role: "Software Developer Intern",
     company: "Studycle",
     period: "Aug 2021 - Apr 2022",
     description:
-      "Develop the company's website using Next.js, TailwindCSS, and Ant Design components.",
+      "Developed the front-end of a learning system website for an institute based in Singapore.",
+    tools: "ReactJS, TailwindCSS",
   },
   {
     role: "Teaching Assistant of Data Science & Database",
     company: "Universitas Indonesia",
     period: "Aug 2021 - Apr 2022",
-    description:
-      "Develop the company's website using Next.js, TailwindCSS, and Ant Design components.",
+    description: "Tutoring and assess student's assignment",
+    tools: "Python, PostgreSQL, diagrams.net",
   },
 ];
--- src/components/home/ProjectSection.jsx
+++ src/components/home/ProjectSection.jsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import CardProject from "../CardProject";
 
 const ProjectSection = () => {
   return (
     <section className="mb-8 w-full">
-      <h2 className="py-2 px-4 my-4 bg-theme2 rounded-full border drop-shadow-lg text-2xl">
-        Highlighted Projects
-      </h2>
+      <div className="my-8 flex items-center justify-between rounded-full border bg-theme2 py-2 px-4  drop-shadow-lg">
+        <h2 className="text-2xl">Highlighted Projects</h2>
+        <Link href="/projects">View more</Link>
+      </div>
       {/* <ul className="pl-8 mb-4 list-disc">
         {skills.map((skill) => (
           <li key={skill}>{skill}</li>
         ))}
       </ul> */}
-      <ul className="grid grid-cols-2 gap-10 pl-2">
+      <ul className="grid gap-4 pl-2 md:grid-cols-3">
         {projects.map(({ title, date, desc, tools, webLink, repoLink }) => (
           <CardProject
             key={title}
@@ -34,10 +36,26 @@ export default ProjectSection;
 
 const projects = [
   {
-    title: "Project 1",
-    date: "Jan, 2021",
-    desc: "Lorem ipsum dolor sit amet",
+    title: "Learning System",
+    date: "2021",
+    desc: "Learning system web for an institute in Singapore",
     tools: "ReactJS, TailwindCSS",
+    webLink: "/",
+    repoLink: "/",
+  },
+  {
+    title: "UMKM Platform",
+    date: "2021",
+    desc: "Website for applying enterprise (UMKM) permission.",
+    tools: "ReactJS, TailwindCSS, Django REST Framework",
+    webLink: "/",
+    repoLink: "/",
+  },
+  {
+    title: "MatkulPanda",
+    date: "2020",
+    desc: "Website used for read and give testimonies on courses in Fasilkom UI",
+    tools: "HTML, CSS, Bootstrap, Django",
     webLink: "/",
     repoLink: "/",
   },
