55de9911a2425573024aab8f70526c5f25aa11aa
yasminadelia <yasminadelia11@gmail.com>
2023-10-06 18:17:18 +0700

    style: change home button

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
 import React from "react";
 import { ButtonSys } from "../buttons";
+import { HomeIcon } from "../icons";
 
 const Header = () => {
   return (
-    <nav className="flex h-20 items-center rounded-b-xl border-b border-black bg-theme3 px-12 drop-shadow-lg">
-      <ul className="justify-left grid grid-flow-col">
+    <nav className="flex h-20 items-center justify-between rounded-b-xl border-b border-black bg-theme3 px-12 drop-shadow-lg ">
+      <Link href="/" className="rounded-lg p-2 hover:bg-theme2">
+        <HomeIcon />
+      </Link>
+      <ul className="grid grid-flow-col">
         {links.map(({ label, href }) => (
           <li key={label}>
-            <ButtonSys className="mr-7 whitespace-nowrap" bgColor={"bg-theme2"}>
+            <ButtonSys className="mr-2 whitespace-nowrap" bgColor={"bg-theme2"}>
               <Link
                 href={href}
                 className="font-semibold text-black no-underline hover:text-black hover:no-underline"
               >
-                {label}
+                <h5>{label}</h5>
               </Link>
             </ButtonSys>
           </li>
@@ -24,7 +28,6 @@ const Header = () => {
 };
 
 const links = [
-  { href: "/", label: "Home" },
   { href: "/projects", label: "Projects" },
   { href: "/about", label: "About Me" },
 ];
